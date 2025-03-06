aba9ac73cec098c9564e557231d91f3fb058f0a1
Shamuskado22 <samucaoliveira1995@gmail.com>
2025-03-06 11:47:06 -0300

    Operadores finalizados

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,79 @@
+public class Operadores {
+    public static void main(String[] args) {
+        // Resto de divisão
+        // int modulo = 15 % 6;
+        // System.out.println(modulo);
+
+        // Negativo, Positivo, Decremento e Incremento
+        // int numero = 5;
+        // numero = - numero;
+
+        // System.out.println(numero);
+
+        // numero = numero * -1;
+
+        // System.out.println(numero);
+
+        // int numero = 5;
+        // numero--;
+
+        // System.out.println(numero);
+
+        // Negação
+        // boolean variavel = true;
+
+        // System.out.println(!variavel);
+
+        // variavel = !variavel;
+
+        // System.out.println(!variavel);
+
+        // Ternário
+        // int a, b;
+        // a = 6;
+        // b = 5;
+        // String resultado = a == b ? "Verdadeiro" : "Falso";
+
+        // System.out.println(resultado);
+
+        // Relacional
+        // int numero1 = 1;
+        // int numero2 = 2;
+
+        // boolean simNao = numero1 == numero2;
+
+        // System.out.println("número 1 é igual ao número 2? " + simNao);
+
+        // simNao = numero1 != numero2;
+
+        // System.out.println("número 1 é diferente de número 2? " + simNao);
+
+        // simNao = numero1 > numero2;
+
+        // System.out.println("número 1 é maior que o número 2? " + simNao);
+
+        // simNao = numero1 < numero2;
+
+        // System.out.println("número 1 é menor que o número 2? " + simNao);
+
+        // String nomeUm = "Samuel";
+        // String nomeDois = new String("Samuel");
+
+        // System.out.println(nomeUm == nomeDois);
+
+        // Lógicos
+        // boolean condicao1 = true;
+        // boolean condicao2 = false;
+        // boolean condicao3 = true;
+
+        // if (condicao1 || (condicao2 && condicao3)) {
+        //     System.out.println("As condições são verdadeiras");
+        // }
+
+        // if (condicao1 && (condicao2 || condicao3)) {
+        //     System.out.println("Uma das condições é verdadeira");
+        // }
+
+        // System.out.println("Fim");
+    }
+}
